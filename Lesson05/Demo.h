6ddff56c3c5d93751807396157e44053efd55e69
Muhammad Imam Hidayat <87066426+mimam-h@users.noreply.github.com>
2022-10-15 23:40:37 +0700

    Update Demo.h

--- Lesson05/Demo.h
+++ Lesson05/Demo.h
@@ -15,20 +15,26 @@ public:
 private:
 	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
 	float angle = 0;
-	float scale = 1;
-	float size = 0.0050f;
-	float pos = 0;
-	float** wayPoint;
-	int i = 0;
+	float view_angle = 0;
+	float post = 0;
+	//float scale = 1;
+	//float size = 0.0050f;
+	float** wayPoints;
+	int wayPointIndex=0;
+	glm::vec3 currentPost = {0,0,0};
+	glm::vec3 nextPost;
+	glm::vec3 beforeCurrentPost {0,0,5};
+
 	virtual void Init();
 	virtual void DeInit();
 	virtual void Update(double deltaTime);
 	virtual void Render();
-	virtual void ProcessInput(GLFWwindow* window);
+	virtual void ProcessInput(GLFWwindow *window);
 	void BuildColoredCube();
 	void BuildColoredPlane();
+	float** GenerateBezier(int segmentCount);
+	float findAngle(glm::vec3 a0, glm::vec3 a1);
 	// void DrawColoredCube(glm::vec3 translate_vector, glm::vec3 rotate_vector, glm::vec3 scale_vector, float rotate_angle);
 	void DrawColoredCube(glm::vec3 translate_vector, glm::vec3 rotate_vector, glm::vec3 scale_vector, float rot_angle, bool rotate);
 	void DrawColoredPlane();
-	float** GenerateBezier();
 };
